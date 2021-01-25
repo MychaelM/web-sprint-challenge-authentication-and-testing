d9fb15f1b62995c55f22aaf05bbd2c0b65f06e89
Mychael M <mychaelbmenges@gmail.com>
2021-01-24 17:37:31 -0700

    added tests for register endpoint

--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,21 @@
-// Write your tests here
+const supertest = require("supertest")
+const server = require("../api/server")
+
 test('sanity', () => {
   expect(true).toBe(false)
+})
+
+describe("tests auth integration", () => {
+  it("successfully registers new user", async () => {
+    const res = await supertest(server)
+      .post("/register")
+      .send({
+        username: "test",
+        password: "abc123"
+      })
+
+      expect(res.statusCode).toBe(201)
+      expect(res.type).toBe("application/json")
+      expect(res.body.username).toBe("test")
+  })
 })
